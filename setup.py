c4f4769b775acc775478139c115a644c476d4618
Melika Qahqaie <melika.qh@gmail.com>
2022-12-02 11:55:47 +0100

    correction
    corrected some typing mistakes

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name = "snowflake",
+    version="0.1",
+    author="Melika",
+    author_email="melika.qahqaie@fau.de",
+    packages=find_packages(),
+    install_requires=["numpy", "turtles"]
+)
+
+
